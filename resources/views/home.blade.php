fa49e6d772c76e3c1c6401413ad737832a84aa0d
ghinnewa <78138649+ghinnewa@users.noreply.github.com>
2023-08-17 16:06:40 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -905,9 +905,11 @@
 <!-- jQuery UI 1.11.4 -->
 <script src={{ asset("assets/plugins/jquery-ui/jquery-ui.min.js") }}></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<!-- <script>
+ <script>
+       $.widget.bridge('uibutton', $.ui.button)
+
 </script>
- -->
+
 <!-- Bootstrap 4 -->
 <script src={{ asset("assets/plugins/bootstrap/js/bootstrap.bundle.min.js") }}></script>
 <!-- ChartJS -->
