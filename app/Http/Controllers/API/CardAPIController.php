42d1c5322b096ca77623ddacb1ade8a859f39372
ghinnewa <78138649+ghinnewa@users.noreply.github.com>
2023-08-17 12:13:32 +0200

    Update CardAPIController.php
    test

--- app/Http/Controllers/API/CardAPIController.php
+++ app/Http/Controllers/API/CardAPIController.php
@@ -67,7 +67,7 @@ class CardAPIController extends AppBaseController
 
         DB::beginTransaction();
         $input = $request->all();
-        $input['image'] = $this->cardRepository->files($request->image, 'profile');
+        // $input['image'] = $this->cardRepository->files($request->image, 'profile');
         $input['identity_file1'] = $this->cardRepository->files($request->identity_file1, 'identity_file1');
         $input['identity_file2'] = $this->cardRepository->files($request->identity_file2, 'identity_file2');
 
